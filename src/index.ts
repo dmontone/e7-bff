9bd8445c3cf7e6c46633a4a28b1da347a62223e2
d m <dmontone@gmail.com>
2024-06-04 13:18:03 -0300

    adds monitoring tool

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import { router } from './router'
+import Monitor from 'express-status-monitor'
 
 const app = express()
+const monitor = Monitor()
 
 app.use(router)
+app.use(monitor)
 
 app.listen(3000, () => console.log('listening on port 3000'))
