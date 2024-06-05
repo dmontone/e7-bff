6c034749410e216a0345f8e3ba845f593aa6e772
d m <dmontone@gmail.com>
2024-06-05 02:26:46 -0300

    adds business logics - active filter and location map

--- src/modules/jobs/controller.ts
+++ src/modules/jobs/controller.ts
@@ -5,12 +5,17 @@ export type JobsController = {
   get: RequestHandler
 }
 
+const filterActive = (job: Job) => job.is_active
+const mapLocation = (job: Job) => ({ ...job, location: job.location || 'remoto' })
+
 const get: RequestHandler = async (_, res, next) => {
   const svc = JobsService()
   const { jobs } = await svc.get()
 
   res.locals.status = 200
   res.locals.data = jobs
+    .filter(filterActive)
+    .map(mapLocation)
 
   next()
 }
